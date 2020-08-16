631e75fae4f7917532fa182f19fe8c547cb6551f
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-08-16 16:45:09 +0300

    Add basic project structure; rename the main controller

--- src/main/java/eimontaite/aiste/vehicleleasingapi/LeasingApplication.java
+++ src/main/java/eimontaite/aiste/vehicleleasingapi/LeasingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VehicleleasingapiApplication {
+public class LeasingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VehicleleasingapiApplication.class, args);
+		SpringApplication.run(LeasingApplication.class, args);
 	}
 
 }
