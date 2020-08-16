dfaff22c5707288ddb2adf475978b421923daf6d
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-08-16 16:53:58 +0300

    Add the Application repository interface

---
+++ src/main/java/eimontaite/aiste/vehicleleasingapi/repositories/ApplicationRepository.java
@@ -0,0 +1,8 @@
+package eimontaite.aiste.vehicleleasingapi.repositories;
+
+import eimontaite.aiste.vehicleleasingapi.models.Application;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationRepository extends CrudRepository<Application, Long> {
+
+}
