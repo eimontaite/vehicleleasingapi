83dbee19a0751affb4239d97f5fe0a04f17651a8
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-08-16 17:29:00 +0300

    Add the logic for determining the application status

--- src/main/java/eimontaite/aiste/vehicleleasingapi/controllers/ApplicationController.java
+++ src/main/java/eimontaite/aiste/vehicleleasingapi/controllers/ApplicationController.java
@@ -18,11 +18,44 @@ public class ApplicationController {
 		this.applicationService = applicationService;
 	}
 
+	enum MaritalStatus {
+		MARRIED,
+		SINGLE
+	}
+
+	enum Status {
+		APPROVED,
+		REJECTED
+	}
+
+	public String determineApplicationStatus(String maritalStatus, int numberOfChildren, int monthlyIncome, long requestedAmount, long leasingPeriod) {
+		int familySize = 1;
+		if (maritalStatus.equals(MaritalStatus.MARRIED.toString())) {
+			familySize++;
+		}
+
+		familySize += numberOfChildren;
+
+		if (((monthlyIncome - requestedAmount / leasingPeriod) / familySize) >= 600) {
+			return Status.APPROVED.toString();
+		} else {
+			return Status.REJECTED.toString();
+		}
+	}
+
 	@PostMapping(value = "/applications", consumes = "application/json", produces = "application/json")
 	public long createApplication(@RequestBody ApplicationDTO applicationDTO) {
 		ModelMapper modelMapper = new ModelMapper();
 		Application application = modelMapper.map(applicationDTO, Application.class);
 
+		application.setStatus(determineApplicationStatus(
+				application.getMaritalStatus(),
+				application.getChildren(),
+				application.getFamilyMonthlyIncome(),
+				application.getRequestedAmount(),
+				application.getLeasingPeriod())
+		);
+		
 		return applicationService.create(application).getId();
 	}
 }
