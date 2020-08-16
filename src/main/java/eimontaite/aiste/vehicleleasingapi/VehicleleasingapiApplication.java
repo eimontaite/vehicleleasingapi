031b2c6f6d51dc05a3adb10bfe448126593aa514
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-08-16 16:38:49 +0300

    Initial commit

---
+++ src/main/java/eimontaite/aiste/vehicleleasingapi/VehicleleasingapiApplication.java
@@ -0,0 +1,13 @@
+package eimontaite.aiste.vehicleleasingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleleasingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleleasingapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eimontaite/aiste/vehicleleasingapi/VehicleleasingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package eimontaite.aiste.vehicleleasingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleleasingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
